febf7174428b76bef1fa273e7ce4b74de63f1e23
Shubham Patil <shubhampatilsd@gmail.com>
2022-04-01 20:04:31 -0700

    upload to github

--- App.tsx
+++ App.tsx
@@ -1,20 +1,72 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from "react-native";
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+function Main({ navigation }) {
+  return (
+    <Tab.Navigator
+      screenOptions={{
+        tabBarActiveTintColor: "#0d9f61",
+      }}
+    >
+      <Tab.Screen
+        name="Home"
+        component={() => {
+          return (
+            <View>
+              <Text>Home Screen</Text>
+              <Button
+                onPress={() => {
+                  navigation.navigate("Settings");
+                }}
+                title="Go to settings"
+              />
+            </View>
+          );
+        }}
+        options={{
+          headerShown: false,
+        }}
+      />
+
+      <Tab.Screen
+        name="OtherPage"
+        component={() => {
+          return <Text>Other Screen</Text>;
+        }}
+        options={{
+          headerShown: false,
+        }}
+      />
+    </Tab.Navigator>
+  );
+}
+
+function Settings() {
+  return <Text style={styles.container}>You got to the settings page</Text>;
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Main" component={Main} />
+        <Stack.Screen name="Settings" component={Settings} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
